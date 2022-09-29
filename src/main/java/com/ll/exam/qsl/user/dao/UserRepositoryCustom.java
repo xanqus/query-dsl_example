57bcafccfd855425d48dcdb0c852e70294939db4
xanqus <sbk98031816@gmail.com>
2022-09-29 13:21:21 +0900

    add JPAQueryFactory object as a bean

--- src/main/java/com/ll/exam/qsl/user/dao/UserRepositoryCustom.java
+++ src/main/java/com/ll/exam/qsl/user/dao/UserRepositoryCustom.java
@@ -1,4 +1,9 @@
 package com.ll.exam.qsl.user.dao;
 
+import com.ll.exam.qsl.user.entity.SiteUser;
+
 public interface UserRepositoryCustom {
+
+
+    SiteUser getQslUser(Long id);
 }
--- src/main/java/com/ll/exam/qsl/user/dao/UserRepositoryImpl.java
+++ src/main/java/com/ll/exam/qsl/user/dao/UserRepositoryImpl.java
@@ -1,4 +1,25 @@
 package com.ll.exam.qsl.user.dao;
 
+import com.ll.exam.qsl.user.entity.QSiteUser;
+import com.ll.exam.qsl.user.entity.SiteUser;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class UserRepositoryImpl implements UserRepositoryCustom{
+    private final JPAQueryFactory jpaQueryFactory;
+    /*
+    SELECT *
+    FROM site_user
+    Where id = 1
+    */
+    @Override
+    public SiteUser getQslUser(Long id){
+        return (SiteUser) jpaQueryFactory
+                .select(QSiteUser.siteUser)
+                .from(QSiteUser.siteUser)
+                .where(QSiteUser.siteUser.id.eq(1L))
+                .fetch();
+    }
+
 }
