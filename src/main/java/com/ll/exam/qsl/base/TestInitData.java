301568c5802690f11ea402505a5a367b423c4539
xanqus <sbk98031816@gmail.com>
2022-09-30 10:29:59 +0900

    implement TestInitData

---
+++ src/main/java/com/ll/exam/qsl/base/TestInitData.java
@@ -0,0 +1,34 @@
+package com.ll.exam.qsl.base;
+
+import com.ll.exam.qsl.user.dao.UserRepository;
+import com.ll.exam.qsl.user.entity.SiteUser;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@Profile("test")
+public class TestInitData {
+    @Bean
+    CommandLineRunner init(UserRepository userRepository) {
+        return args -> {
+            SiteUser u1 = SiteUser.builder()
+                    .username("user1")
+                    .password("{noop}1234")
+                    .email("user1@test.com")
+                    .build();
+
+            SiteUser u2 = SiteUser.builder()
+                    .username("user2")
+                    .password("{noop}1234")
+                    .email("user2@test.com")
+                    .build();
+
+            List<SiteUser> siteUsers = userRepository.saveAll(Arrays.asList(u1, u2));
+        };
+    }
+}
